f98c1a4493e611642493e0e08b5d9a95a675bd35
Sanjay Kumar <sanjubabu9419@gmail.com>
2024-05-02 12:23:10 +0530

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { FaInstagram } from "react-icons/fa";
+import { CiFacebook } from "react-icons/ci";
+import { CiLinkedin } from "react-icons/ci";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaGithubSquare } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+
+const Contact = () => {
+  return (
+    <>
+      <div className="container contact" id="contact">
+        <h1>CONTACT ME</h1>
+        <div
+          className="contact-icon"
+          data-aos="zoom-in-up"
+          data-aos-duration="1000"
+        >
+          <a href="https://www.google.com" target="_blank" className="items">
+            <FaInstagram className="icons" />
+          </a>
+          <a href="https://www.google.com" target="_blank" className="items">
+            <CiFacebook className="icons" />
+          </a>
+          <a href="https://www.linkedin.com/in/sanjay-kumar-3ab6321aa/?trk=opento_sprofile_details" target="_blank" className="items">
+            <CiLinkedin className="icons" />
+          </a>
+          <a href="https://github.com/SanjayKumar515" target="_blank" className="items">
+            <FaGithubSquare className="icons" />
+          </a>
+          <a
+            href="mailto:sanjubabu9419@gmail.com"
+            target="_blank"
+            className="items"
+          >
+            <SiGmail className="icons" />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
